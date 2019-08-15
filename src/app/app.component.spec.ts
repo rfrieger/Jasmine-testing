f026a70a6ab16529ada921c1e66e9eb2de46185d
rfrieger <rfrieger@gmail.com>
2019-08-14 20:08:50 -0400

    practice with few simple jasmine test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,27 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-unit-testing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-unit-testing');
+    expect(app.title).toEqual('Angular Unit Testing');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-unit-testing!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular Unit Testing!');
+  });
+
+  it('should have alt Angular Logo in img tag', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('img').alt).toContain('Angular Logo');
+  });
+
+  it('should have Tour of Heroes in h2', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').alt).toContain('Tour of Heroes');
   });
 });
